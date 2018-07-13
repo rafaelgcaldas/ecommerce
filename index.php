080ddb36772605dd7bb336ad70994e5f90529de3
Your Name <rafaelzinhogomes22@live.com>
2018-07-12 22:05:09 -0300

    Template Admin

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	require_once("vendor/autoload.php");
 	use \Slim\Slim;
 	use \Hcode\Page;
+	use \Hcode\PageAdmin;
 
 	$app = new Slim();
 
@@ -9,6 +10,11 @@
 
 	$app->get('/', function() {
 		$page = new Page();
+		$page->setTpl("index");
+	});
+
+	$app->get('/admin', function() {
+		$page = new PageAdmin();
 		$page->setTpl("index");
 	});
 
