4b8bafa78d314fe1d1f03556ec30ac780de435ad
Your Name <rafaelzinhogomes22@live.com>
2018-07-20 10:54:31 -0300

    Paginação de Produtos no Site

--- site.php
+++ site.php
@@ -13,14 +13,26 @@
 		]);
 	});
 
-	$app->get("/categories/:idCategory", function($idCategory) {
-		$category = new Category();
-		$page = new Page();
+	$app->get("/categories/:idcategory", function($idcategory){
+		$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-		$category->get((int)$idCategory);
-		$page->setTpl("category",[
-			"category"=>$category->getValues(),
-			"products"=>Product::checkList($category->getProducts())
+		$category = new Category();
+		$category->get((int)$idcategory);
+		$pagination = $category->getProductsPage($page);
+		$pages = [];
+
+		for ($i=1; $i <= $pagination['pages']; $i++) { 
+			array_push($pages, [
+				'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+				'page'=>$i
+			]);
+		}
+		
+		$page = new Page();
+		$page->setTpl("category", [
+			'category'=>$category->getValues(),
+			'products'=>$pagination["data"],
+			'pages'=>$pages
 		]);
 	});
 ?>
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -82,6 +82,28 @@
             }
         }
 
+        public function getProductsPage($page = 1, $itemsPerPage = 8){
+            $sql = new Sql();
+            $start = ($page - 1) * $itemsPerPage;
+
+            $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+                        FROM tb_products a
+                        INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                        INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+                        WHERE c.idcategory = :idcategory
+                        LIMIT $start, $itemsPerPage;", array(
+                            ":idcategory"=>$this->getidcategory()
+                        ));
+
+            $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrTotal;");
+
+            return [
+                "data"=>Product::checkList($results),
+                "total"=>(int)$resultTotal[0]["nrTotal"],
+                "pages"=>ceil($resultTotal[0]["nrTotal"] / $itemsPerPage)
+            ];
+        }
+
         public function addProduct(Product $product){
             $sql = new Sql();
             $sql->select("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES(:idcategory, :idproduct)",array(
