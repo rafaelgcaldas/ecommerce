3637aa36d3984138d885d1f55117f080d3415592
Your Name <rafaelzinhogomes22@live.com>
2018-08-01 17:50:17 -0300

    Web Service de Endereço

--- site.php
+++ site.php
@@ -108,15 +108,83 @@
 
 	$app->get("/checkout", function(){
 		User::verifyLogin(false);
-
-		$cart = Cart::getFromSession();
 		$address = new Address();
+		$cart = Cart::getFromSession();
+
+		if(isset($_GET["zipcode"])){
+			$_GET["zipcode"] = $cart->getdeszipcode();
+		}
+
+		if(isset($_GET["zipcode"])){
+			$address->loadFromCep($_GET["zipcode"]);
+			$cart->setdeszipcode($_GET["zipcode"]);
+			$cart->save();
+			$cart->getCalculateTotal();
+		}
+
+		if(!$address->getdesaddress()) $address->setdesaddress("");
+		if(!$address->getdescomplement()) $address->setdescomplement("");
+		if(!$address->getdesdistrict()) $address->setdesdistrict("");
+		if(!$address->getdescity()) $address->setdescity("");
+		if(!$address->getdesstate()) $address->setdesstate("");
+		if(!$address->getdescountry()) $address->setdescountry("");
+		if(!$address->getdeszipcode()) $address->setdeszipcode("");
+
 		$page = new Page();
 
 		$page->setTpl("checkout", [
 			"cart"=>$cart->getValues(),
-			"address"=>$address->getValues()
+			"address"=>$address->getValues(),
+			"products"=>$cart->getProducts(),
+			"error"=>Address::getMsgError()
 		]);
+	});
+
+	$app->post("/checkout", function(){
+		User::verifyLogin(false);
+		$user = User::getFromSession();
+		$address = new Address();
+
+		if(!isset($_POST["zipcode"]) || $_POST["zipcode"] === ""){
+			Address::setMsgError("Informe o Cep!");
+			header("Location: /checkout");
+			exit;
+		}
+
+		if(!isset($_POST["desaddress"]) || $_POST["desaddress"] === ""){
+			Address::setMsgError("Informe o Endereço!");
+			header("Location: /checkout");
+			exit;
+		}
+		if(!isset($_POST["desdistrict"]) || $_POST["desdistrict"] === ""){
+			Address::setMsgError("Informe o Bairro!");
+			header("Location: /checkout");
+			exit;
+		}
+		if(!isset($_POST["descity"]) || $_POST["descity"] === ""){
+			Address::setMsgError("Informe a Cidade!");
+			header("Location: /checkout");
+			exit;
+		}
+		if(!isset($_POST["desstate"]) || $_POST["desstate"] === ""){
+			Address::setMsgError("Informe o Estado!");
+			header("Location: /checkout");
+			exit;
+		}
+		if(!isset($_POST["descountry"]) || $_POST["descountry"] === ""){
+			Address::setMsgError("Informe o País!");
+			header("Location: /checkout");
+			exit;
+		}
+
+		$_POST["deszipcode"] = $_POST["zipcode"];
+		$_POST["idperson"] = $user->getidperson();
+
+		$address->setData($_POST);
+		$address->save();
+
+		header("Location: /order");
+		exit;
 	});
 
 	$app->get("/login", function(){
--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -5,5 +5,71 @@
 
     class Address extends Model{
 
+        const SESSION_ERROR = "AddressError";
+        
+        public static function getCep($nrCep){
+
+            $nrCep = str_replace("-","", $nrCep);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://viacep.com.br/ws/$nrCep/json/");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+            $data = json_decode(curl_exec($ch), true);
+
+            curl_close($ch);
+
+            return $data;
+        }
+
+        public function loadFromCep($nrCep){
+
+            $data = Address::getCep($nrCep);
+
+            if(isset($data["localidade"]) && $data["localidade"]){
+                $this->setdesaddress($data["logradouro"]);
+                $this->setdescomplement($data["complemento"]);
+                $this->setdesdistrict($data["bairro"]);
+                $this->setdescity($data["localidade"]);
+                $this->setdesstate($data["uf"]);
+                $this->setdescountry("Brasil");
+                $this->setdeszipcode($nrCep);
+            }
+        }
+
+        public function save(){
+            $sql = new Sql();
+
+            $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :complement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", array(
+                "idaddress"=>$this->getidaddress(),
+                "idperson"=>$this->getidperson(),
+                "desaddress"=>utf8_decode($this->getdesaddress()),
+                "complement"=>utf8_decode($this->getcomplement()),
+                "descity"=>utf8_decode($this->getdescity()),
+                "desstate"=>utf8_decode($this->getdesstate()),
+                "descountry"=>utf8_decode($this->getdescountry()),
+                "deszipcode"=>$this->getdeszipcode(),
+                "desdistrict"=>utf8_decode($this->getdesdistrict())
+            ));
+
+            if($results > 0){
+                $this->setData($results[0]);
+            }
+        }
+
+        public static function setMsgError($msg){
+            $_SESSION[Address::SESSION_ERROR] = $msg;
+        }
+
+        public static function getMsgError(){
+            $msg =  (isset($_SESSION[Address::SESSION_ERROR])) ? $_SESSION[Address::SESSION_ERROR] : "";
+            Address::clearMsgError();
+            return $msg;
+        }
+
+        public static function clearMsgError(){
+            $_SESSION[Address::SESSION_ERROR] = NULL;
+        }
     }
 ?>
