5511aa86ac72b72d3619a1b5d31638e45784393c
Your Name <rafaelzinhogomes22@live.com>
2018-08-03 00:08:31 -0300

    Admin - Pedidos

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@
         return number_format($vlPrice, 2, ",", ".");
     }
 
+    function formatDate($date){
+        return date("d/m/Y", strtotime($date));
+    }
+
     function checkLogin($inadmin = true){
         return User::checkLogin($inadmin);
     }
